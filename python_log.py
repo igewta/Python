9d1f8c828a00fb789642e04bdb01d1e3aedf3fed
thinkpad <igewta@foxmail.com>
2020-07-11 19:37:31 +0800

    nothing

---
+++ python_log.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+"""
+文件说明：
+用来学习logging模块
+"""
+import logging
+'''
+logging.debug("this is debug message")
+logging.info("this is info message")
+logging.warning("this is warning message")
+logging.critical("this is critical message")
+logging.log("debug","this is test debug message")
+'''
+logging.basicConfig(.)
+
