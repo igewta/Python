92a7972e975b0eb1728fd9e2008714dd7893c3d3
thinkpad <igewta@foxmail.com>
2020-08-11 21:06:18 +0800

    tkinter

---
+++ tuling/Tkinter/test_tk.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+文件说明：
+
+"""
+import tkinter
+
+# tkinter._test()
+base = tkinter.Tk()
+
+# 消息循环
+base.mainloop()
+'''
+- 按钮
+    Button 按钮组件
+    RadioButton 单选框组件
+    CheckButton 复选框组件
+    Listbox 列表框组件
+- 文本输入组件
+    Entry 单行文本框组件
+    Text 多行文本框组件
+- 标签组件
+    Label 标签组件，可以显示图片和文字
+    Message 标签组件，可以根据内容将文字换行
+- 菜单
+    Menu 菜单组件
+    MenuButton 菜单按钮组件，可以使用Menu 代替
+- 滚动条
+    Scale 滑块组件
+    Scrollbar 滚动条组件
+- 其他组件
+    Canvas 画布组件
+    Frame 框架组件，将多个组件编组
+    Toplevel 创建子窗口容器组件  
+    
+ 
+'''
